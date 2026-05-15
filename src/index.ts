016f73016f386d8c1c4ce869e0c557e81ffe59dd
Rodrigo Muro Barajas <rodrigomurob1997@gmail.com>
2026-05-14 18:17:31 -0600

    chore: remove legacy validate module and finalize index migration

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,49 @@
 import { FormValidator } from "./components/FormValidator.js";
-import { Card } from "./components/Card.js";
+import { Card, CardData } from "./components/Card.js";
 import { Section } from "./components/Section.js";
 import { UserInfo } from "./components/UserInfo.js";
 import { PopupWithImage } from "./components/PopupWithImage.js";
 import { PopupWithForm } from "./components/PopupWithForm.js";
 import { defaultFormConfig, initialCards } from "./utils/constants.js";
 
-const editProfileButton = document.querySelector(".profile__edit-button");
-const editProfileModal = document.querySelector("#edit-popup");
+const editProfileButton = document.querySelector(
+  ".profile__edit-button"
+) as HTMLButtonElement;
+const editProfileModal = document.querySelector("#edit-popup") as HTMLElement;
 
-const editProfileForm = editProfileModal.querySelector(".popup__form");
-const nameInput = editProfileModal.querySelector(".popup__input_type_name");
+const editProfileForm = editProfileModal.querySelector(
+  ".popup__form"
+) as HTMLFormElement;
+const nameInput = editProfileModal.querySelector(
+  ".popup__input_type_name"
+) as HTMLInputElement;
 const descriptionInput = editProfileModal.querySelector(
-  ".popup__input_type_description",
-);
+  ".popup__input_type_description"
+) as HTMLInputElement;
 
-const addCardButton = document.querySelector(".profile__add-button");
-const addCardModal = document.querySelector("#new-card-popup");
-const addCardForm = addCardModal.querySelector(".popup__form");
+const addCardButton = document.querySelector(
+  ".profile__add-button"
+) as HTMLButtonElement;
+const addCardModal = document.querySelector("#new-card-popup") as HTMLElement;
+const addCardForm = addCardModal.querySelector(
+  ".popup__form"
+) as HTMLFormElement;
 
 const editProfileFormValidator = new FormValidator(
   defaultFormConfig,
-  editProfileForm,
+  editProfileForm
 );
 
 const addCardFormValidator = new FormValidator(defaultFormConfig, addCardForm);
 
-const cardSection = new Section(
+const cardSection = new Section<CardData>(
   {
     items: initialCards.slice().reverse(),
-    renderer: (card) => {
+    renderer: (card: CardData) => {
       renderCard(card);
     },
   },
-  ".cards__list",
+  ".cards__list"
 );
 
 const userInfo = new UserInfo({
@@ -53,7 +63,7 @@ const editProfilePopup = new PopupWithForm("#edit-popup", (inputValues) => {
 });
 
 const addCardPopup = new PopupWithForm("#new-card-popup", (inputValues) => {
-  const cardData = {
+  const cardData: CardData = {
     name: inputValues["place-name"],
     link: inputValues.link,
   };
@@ -62,35 +72,35 @@ const addCardPopup = new PopupWithForm("#new-card-popup", (inputValues) => {
   addCardPopup.close();
 });
 
-function fillProfileForm() {
+function fillProfileForm(): void {
   const profileData = userInfo.getUserInfo();
 
   nameInput.value = profileData.name;
   descriptionInput.value = profileData.description;
 }
 
-function handleCardClick(name, link) {
+function handleCardClick(name: string, link: string): void {
   imagePopup.open(name, link);
 }
 
-function handleOpenEditModal() {
+function handleOpenEditModal(): void {
   fillProfileForm();
   editProfileFormValidator.resetValidation();
   editProfilePopup.open();
 }
 
-function handleOpenAddCardModal() {
+function handleOpenAddCardModal(): void {
   addCardForm.reset();
   addCardFormValidator.resetValidation();
   addCardPopup.open();
 }
 
-function createCard(data) {
+function createCard(data: CardData): HTMLElement {
   const card = new Card(data, "#card-template", handleCardClick);
   return card.generateCard();
 }
 
-function renderCard(data) {
+function renderCard(data: CardData): void {
   const cardElement = createCard(data);
   cardSection.addItem(cardElement);
 }
